7f6536787f6619cfe5c2e3613ad71cea9811650f
KanaVahini <kanavahini@gmail.com>
2026-07-16 11:39:41 +0530

    july 16 26 que 8

--- binary_search.cpp
+++ binary_search.cpp
@@ -45,3 +45,51 @@ int main() {
 
     return 0;
 }
+
+/*Binary Search using recursion
+
+class Solution{
+private:
+    int func(vector<int>&nums, int low, int high, int target){
+        if(low>high){
+            return -1;
+        }
+        int ind;
+        int mid = (low+high)/2;
+        if(nums[mid] == target){
+            ind = mid;
+        }
+        else if(nums[mid]<target){
+            ind = func(nums,mid+1,high,target);
+        }
+        else{
+            ind = func(nums,low,mid-1,target);
+        }
+        return ind;
+    }
+public:
+    int search(vector<int> &nums, int target){
+        int n = nums.size();
+        int res = func(nums,0,n-1,target);
+        return res;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
